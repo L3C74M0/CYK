d0e489e89587398c2eb79b07069306562fb58f10
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2020-11-16 15:27:36 -0500

    first commit

---
+++ src/ui/Main.java
@@ -0,0 +1,23 @@
+package ui;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("bichoController.fxml"));
+		Scene scene = new Scene(root);
+		stage.setTitle("CYK");
+		stage.setScene(scene);
+		stage.show();
+		stage.setResizable(false);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/ui/bichoController.java
@@ -0,0 +1,32 @@
+package ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class bichoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField grammar;
+
+    @FXML
+    private TextField w;
+
+    @FXML
+    void generate(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
