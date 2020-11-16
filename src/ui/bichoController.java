bc4807a97263f44081fcf7d98b23d7ede99e9a66
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2020-11-16 15:47:20 -0500

    Added CYK

--- src/ui/bichoController.java
+++ src/ui/bichoController.java
@@ -4,7 +4,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
+import javafx.stage.StageStyle;
 
 public class bichoController {
 
@@ -22,7 +25,22 @@ public class bichoController {
 
     @FXML
     void generate(ActionEvent event) {
-    	
+    	boolean is = false;
+    	if(is) {
+    		Alert info = new Alert(AlertType.CONFIRMATION);
+			info.setTitle("ERROR");
+			info.setHeaderText(null);
+			info.initStyle(StageStyle.UTILITY);
+			info.setContentText("Si la genera");
+			info.show();
+    	}else {
+    		Alert info = new Alert(AlertType.ERROR);
+			info.setTitle("ERROR");
+			info.setHeaderText(null);
+			info.initStyle(StageStyle.UTILITY);
+			info.setContentText("No la genera");
+			info.show();
+    	}
     }
 
     @FXML
